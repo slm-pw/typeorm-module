2ac7c9d0ec4df15ece835f5d44033ee6c7e792ab
ruslannkot <aquaaqua6858@gmail.com>
2023-07-01 16:06:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var typeorm = require("typeorm")
+
+let fs = require('fs')
+
+async function getEntities() {
+    let entities = []
+    let files = fs.readdirSync(`${__dirname}/..`)
+    for (let file of files) {
+        if (fs.lstatSync(`${__dirname}/../${file}`).isDirectory()) {
+            let folderFiles = fs.readdirSync(`${__dirname}/../${file}/entities`)
+            for (let folderFile of folderFiles) {
+                if (folderFile.endsWith(".js")) {
+                    let entity = require(`${__dirname}/../${file}/entities/${folderFile}`)
+                    entities.push(entity)
+                }
+            }
+        }
+    }
+    return entities
+}
+
+module.exports = function(settings) {
+    settings = {
+        ...settings,
+        db: {
+            type: "sqlite",
+            database: `${__dirname}/db.sqlite`,
+            entities: [ ...getEntities() ],
+            logging: true,
+            ...settings.db
+          },
+    }
+    clientMixin: (client) => {
+        client.db = new typeorm.DataSource(settings.db)
+        client.db.initialize().then(() => {
+            client.emmit('typeOrmReady')
+        })
+    }
+}
